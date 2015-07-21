2ba15dbc37531752303c7c66a560296390a9bfeb
ratanakso <sor@onid.oregonstate.edu>
2015-07-21 10:49:10 -0600

    Deleted old model.

--- source/lib/PTO-Sim/slblocks.m
+++
@@ -1,6 +0,0 @@
-function blkStruct = slblocks   % Required
-
-Browser.Library = 'PTOSim_Lib'; % Name of library file (.slx) w/o extension 
-Browser.Name = 'PTO-Sim';       % Name that will appear in Simulink Browser
-
-blkStruct.Browser = Browser;    % Required
